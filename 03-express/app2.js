825c83fcf30f1cf7255a93268cf5415efcb15708
ankit shiyal <ankitshiyal2005@gmail.com>
2026-04-30 10:55:55 +0530

    express added

---
+++ 03-express/app2.js
@@ -0,0 +1,32 @@
+
+
+
+import express from "express"
+
+const app = express()
+
+app.get("/", (req ,res)=>{
+
+    res.json({
+        message: "this is home page",
+    })
+
+})
+
+app.get("/about", (req ,res)=>{
+
+    res.json({
+        message: "this is about page",
+    })
+
+})
+
+const port = 5000;
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("server running on port", port);
+});
+
