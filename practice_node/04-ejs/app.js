5df35766c1afb44caf344267e80c8f28ea57d017
ankit shiyal <ankitshiyal2005@gmail.com>
2026-05-01 14:36:29 +0530

    added

---
+++ practice_node/04-ejs/app.js
@@ -0,0 +1,50 @@
+
+import express from "express"
+
+const app = express()
+
+app.set("view engine", "ejs")
+
+app.use(express.urlencoded({extended: true}))
+
+let studentList=[
+    {
+        id : 1,
+        name : "ankit"
+    },
+    {
+        id : 2,
+        name: "mohin"
+    }
+]
+
+app.get("/", (req, res) => {
+  res.render("index", { studentList })
+})
+
+app.get("/add", (req, res) => {
+  res.render("add")
+})
+
+app.post("/add", (req, res) => {
+  const { name } = req.body
+
+  const newStudent = {
+    id: new Date().getTime(),
+    name,
+  }
+
+  studentList.push(newStudent);
+
+  res.redirect("/")
+})
+
+const port = 5000
+
+app.listen(port,(err)=>{
+
+    if(err){
+        console.log(err.message)
+    }
+    console.log(`server is running on ${port}`)
+})
