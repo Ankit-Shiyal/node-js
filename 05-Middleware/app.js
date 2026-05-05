7682628e0e8075c7dcdaa2f4ed912e32c6834b48
ankit shiyal <ankitshiyal2005@gmail.com>
2026-05-05 11:38:09 +0530

    middleware

---
+++ 05-Middleware/app.js
@@ -0,0 +1,45 @@
+
+
+import express from "express"
+import helmet from "helmet"
+
+const app = express()
+
+
+// external Middleware 
+// helmet
+app.use(helmet())
+
+// application Middleware
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("this is home page from server")
+})
+
+// routes level Middleware
+
+app.get("/about", (req, res) => {
+    res.send(" this is about page from server")
+})
+
+// undefined routs level Middleware
+
+app.use((req, res) => {
+    res.status(404).send("page not found in server")
+})
+
+// centralized error Middleware
+
+
+
+const port = 5000
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err.message)
+    }
+
+    console.log(`server running on port ${port}`)
+})
