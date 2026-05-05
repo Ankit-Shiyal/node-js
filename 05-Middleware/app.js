1789fe24b2ec63360a91920635e2062ee5704716
ankit shiyal <ankitshiyal2005@gmail.com>
2026-05-05 11:51:47 +0530

    added

--- 05-Middleware/app.js
+++ 05-Middleware/app.js
@@ -33,15 +33,6 @@ app.use((req, res) => {
 // centralized error Middleware
 
 
-app.use((error, req, res, next) => {
-  if (req.headersSent) {
-    next(error);
-  }
-  res
-    .status(error.statusCode || 500)
-    .json({ message: error.message || "internal server error" });
-});
-
 
 
 const port = 5000
