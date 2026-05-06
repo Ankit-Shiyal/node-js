af7a939a78ca9e6bfecc06d0f99d2334cc160ac8
ankit shiyal <ankitshiyal2005@gmail.com>
2026-05-06 14:59:42 +0530

    added

---
+++ practice_node/06-crud_express/app.js
@@ -0,0 +1,48 @@
+
+
+import express from "express"
+import httpError from "./middleware/httpError.js"
+
+const app= express()
+
+app.use(express.json())
+
+const taskList =[
+    {
+        id: 1,
+        task : "Ridding",
+        description : "book ridding on day"
+    },
+    {
+        id: 2,
+        task : "Run",
+        description : "running 1 km"
+    },
+
+]
+
+app.get("/", (req, res)=>{
+    res.send("hello from server")
+})
+
+app.get("/taskList", (req, res)=>{
+    if(taskList.length ===0){
+        return res.status(200).json({
+            message: "Task not available"
+        })
+    }
+
+     res
+    .status(200)
+    .json({message : "task list" , taskList});
+})
+
+const port = 5000
+
+app.listen(port, (err)=>{
+    if(err){
+        console.log(err.message)
+    }
+
+    console.log(`server running on port ${port}`)
+})
