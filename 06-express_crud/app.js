7bb0701757c195299fb519a7686b2e7b937d8275
ankit shiyal <ankitshiyal2005@gmail.com>
2026-05-08 13:13:06 +0530

    added

--- 06-express_crud/app.js
+++ 06-express_crud/app.js
@@ -21,11 +21,11 @@ const taskList =[
 
 ]
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res, next)=>{
     res.send("hello from server")
 })
 
-app.get("/taskList", (req, res)=>{
+app.get("/taskList", (req, res, next)=>{
     if(taskList.length ===0){
         return res.status(200).json({
             message: "Task not available"
@@ -36,6 +36,65 @@ app.get("/taskList", (req, res)=>{
     .status(200)
     .json({message : "task list" , taskList});
 })
+
+
+app.get("/taskList/:id", (req, res, next) => {
+  const id = Number(req.params.id);
+
+  const task = taskList.find((t) => t.id === id);
+
+  if (!task) {
+    return res
+      .status(404)
+      .json({ success: true, message: "no taskData found with this id" });
+  }
+
+  res.status(200).json({ success: true, message: "task found", task });
+
+});
+
+app.post("/addTask", (req, res, next) => {
+  const { task, description } = req.body;
+
+  if (!task || !description) {
+    return next(new httpError("task or description data are required", 400));
+  }
+
+  const newTask = {
+    id: new Date().getTime(),
+    task,
+    description,
+  };
+
+  taskList.push(newTask);
+
+  res
+    .status(201)
+    .json({ success: true, message: "new Task added successfully", newTask });
+});
+
+
+
+
+app.delete("/taskList/:id", (req, res, next) => {
+
+    const id = Number(req.params.id)
+
+    const index = taskList.findIndex((t) => t.id === id)
+
+    if(index === -1){
+        return next(new httpError("requested route not found", 404))
+    }
+
+    taskList.splice(index, 1)
+
+    res.status(200).json({
+        success: true,
+        message: "task data deleted successfully"
+    })
+
+})
+
 
 
 app.use((req, res, next)=>{
--- 06-express_crud/middleware/httpError.js
+++ 06-express_crud/middleware/httpError.js
@@ -1,8 +1,8 @@
-class HttpError extends Error {
+class httpError extends Error {
   constructor(message, statusCode) {
     super(message);
     statusCode = statusCode;
   }
 }
 
-export default HttpError;
+export default httpError;
