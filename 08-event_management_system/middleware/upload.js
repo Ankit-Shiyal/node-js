ab0b70106ad988863be15b7e2c30a6cc20a614a8
ankit shiyal <ankitshiyal2005@gmail.com>
2026-05-27 12:10:53 +0530

    added

--- 08-event_management_system/middleware/upload.js
+++ 08-event_management_system/middleware/upload.js
@@ -6,16 +6,16 @@ import path from "path";
 import HttpError from "./HttpError.js";
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads");
-  },
+    destination: (req, file, cb) => {
+        cb(null, "uploads");
+    },
 
- filename: function (req, file, cb) {
+    filename: function (req, file, cb) {
 
-    const EventImages = req.file?.req.files.map((file) => file.path) || null
-    const EventPoster= req.file?.req.files[0].path || null
-    const EventSpiker = req.file?.req.files.map((file) => file.path) || null
- }
+        const EventImages = req.file?.req.files.map((file) => file.path) || null
+        const EventPoster = req.file?.req.files[0].path || null
+        const EventSpiker = req.file?.req.files.map((file) => file.path) || null
+    }
 
 
 })
