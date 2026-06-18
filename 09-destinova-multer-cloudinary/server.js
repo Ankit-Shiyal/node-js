aa126f2ca8d99e163a0b5dbe515ba2bf1fcd1ce6
ankit shiyal <ankitshiyal2005@gmail.com>
2026-06-18 11:45:21 +0530

    destinova initial setup added

--- 09-destinova-multer-cloudinary/server.js
+++ 09-destinova-multer-cloudinary/server.js
@@ -1,68 +1,46 @@
-import express from "express"
+import express from "express";
 import dotenv from "dotenv";
 
 dotenv.config({ path: "./.env" });
 
-
 import HttpError from "./middlewares/HttpError.js"
-import router from "./router/packageRouter.js";
 import connectDB from "./config/db.js";
 
-
-const app = express()
-
-app.use(express.json());
-
-app.use("/package", router);
+const app = express();
 
 app.get("/", (req, res) => {
-    res.json({ message: "hello from server" })
+    req.json({ message: "Hello from server" })
 })
 
 app.use((req, res, next) => {
-
-    return next(new HttpError("requested routs not found", 404))
+    return next(new HttpError("requested route not found", 404))
 })
 
 app.use((error, req, res, next) => {
-
     if (res.headersSent) {
         return next(error)
     }
-
-    res.status(error.statusCode || 500).json({
-        message: error.message || "something want wrong please try again"
-    })
-
+    res.status(error.statusCode || 500).json({ message: error.message || "internal server error" })
 })
 
 const port = 5000
 
 async function startServer() {
     try {
-
-        const connect = await connectDB()
-
+        const connect = await connectDB();
         if (!connect) {
-            throw new Error("filed to connect")
+            return console.log("failed to connect db")
         }
-
-
         app.listen(port, (err) => {
             if (err) {
                 return console.log(err.message)
             }
-
             console.log(`server running on port ${port}`)
         })
 
-
     } catch (error) {
         console.log(error.message)
-
         process.exit(1)
-
     }
 }
-
 startServer()
